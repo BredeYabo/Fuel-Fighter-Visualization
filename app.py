bf41c7db4567e7fa8fa6c59efe21f10d543a416a
Brede Yabo Kristensen <Bredeyabo@hotmail.com>
2018-02-27 18:30:26 +0100

    graph test

--- app.py
+++ app.py
@@ -8,14 +8,37 @@ server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2('Fuel Fighter Visualizer'),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['Speed', 'Accelleration', 'Position']],
+        options=[{'label': i, 'value': i} for i in ['Speed', 'Acceleration', 'Position']],
         value='Meters'
     ),
-    html.Div(id='display-value')
+    html.Div(id='display-value'),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    )
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
